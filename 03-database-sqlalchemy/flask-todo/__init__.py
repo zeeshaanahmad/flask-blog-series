458640f466e7572fcef918091331d82777aef2df
Zeeshan <ahmad.zeeshaan@gmail.com>
2019-08-21 19:29:22 +0300

    add: new, update, delete todo with frontend code and styles

--- 03-database-sqlalchemy/flask-todo/__init__.py
+++ 03-database-sqlalchemy/flask-todo/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, url_for, redirect, jsonify
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from .models import Base, TodoItem
+import json
 
 # Setting up flask application
 app = Flask(__name__)
@@ -18,21 +20,77 @@ session = DBSession()
 @app.route("/")
 def index():
     # Querying database for all the todo items
-    todo_items = TodoItem.query().all()
+    todo_items = session.query(TodoItem).all()
 
     # redering index.html template with todo items retrieved 
     # from the databse
-    return render_template("index.html", todo_items=todo_items)
+    return render_template("index.html", todo_items=[todo_item.serialize for todo_item in todo_items])
 
 
-@app.route("/add_todo", methods=['GET','POST'])
+@app.route("/todo", methods=['GET','POST'])
 def insertTodo():
     try:
-        todo_item_title = request.form['title']
-        new_todo_item = TodoItem(title=todo_item_title)
-        session.add(todo_item_title)
+        if request.method == 'GET':
+            return render_template('new_todo.html')
+        elif request.method == 'POST':
+            todo_item_title = request.form['title']
+            new_todo_item = TodoItem(title=todo_item_title)
+            session.add(new_todo_item)
+            session.commit()
 
-        return redirect(url_for('/'))
-
-    except expression as identifier:
+            return redirect(url_for('index'))
+    except:
         abort(422)
+
+@app.route("/todo", methods=['PUT'])
+def updateTodo():
+    body = request.get_json()
+
+    todo_id = body.get('id', None)
+
+    todo_item = session.query(TodoItem).filter(TodoItem.id==todo_id).first()
+
+    if todo_item is None:
+        abort(404)
+
+    todo_item.checked = body.get('checked')
+
+    session.add(todo_item)
+    session.commit()
+
+    return jsonify({"success": True, "updated_todo": todo_id})
+
+
+@app.route("/todo", methods=['DELETE'])
+def deleteTodo():
+    try:
+        body = request.get_json()
+
+        todo_id = body.get('id', None)
+
+        todo_item = session.query(TodoItem).filter(TodoItem.id==todo_id).first()
+
+        if todo_item is None:
+            abort(404)
+        
+        session.delete(todo_item)
+        session.commit()
+
+        return jsonify({"success": True})
+    except:
+        abort(422)
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return jsonify({"success": False, "message": "No Resource Found"})
+
+
+@app.errorhandler(422)
+def handle_422(error):
+    return jsonify({"success": False, "message": "Unprocessable"})
+
+
+@app.errorhandler(400)
+def handle_400(error):
+    return jsonify({"success": False, "message": "Bad request"})
--- 03-database-sqlalchemy/flask-todo/models.py
+++ 03-database-sqlalchemy/flask-todo/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -8,6 +8,11 @@ class TodoItem(Base):
     __tablename__='todoitem'
     id = Column(Integer, primary_key=True)
     title = Column(String(), nullable=False)
+    checked = Column(Boolean, nullable=False, default=False)
+
+    @property
+    def serialize(self):
+        return {'id': self.id, 'title': self.title, 'checked': self.checked}
 
 engine = create_engine('sqlite:///todo.db')
 Base.metadata.create_all(engine)
